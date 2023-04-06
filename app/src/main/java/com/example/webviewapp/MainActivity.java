216653358b87d6606bf7eeb7b120c3f0c1108d24
a22diako <a22diako@student.his.se>
2023-04-06 11:49:35 +0200

    inte klar

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -24,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        WebView mywebview = findViewById(R.id.my_webview);
+       mywebview.loadUrl("https://www.his.se");
 
         /*
         * Rename your App. Tip: Values->Strings
